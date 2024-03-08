47cbba14b085bfe430c16b7d2341e4d77157ca57
Mateusz Górniak <s28490@pjwstk.edu.pl>
2024-03-08 13:22:17 +0100

    add new method

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,6 +2,20 @@
 
 Console.WriteLine("Hello, World!!!, WOOOOOW");
 
+int[] arr = { 1, 2, 3, 4, 5 };
+Console.WriteLine(GetAvg(arr));
+
 String mama = "Bozena";
 String Tata = "Piotr";
 String siostra = "Paulina";
+
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum / arr.Length;
+}
