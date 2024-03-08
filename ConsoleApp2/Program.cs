b3554983ba14054d1579125315c2aff96298f5a4
Mateusz Górniak <s28490@pjwstk.edu.pl>
2024-03-08 12:58:59 +0100

    Modyfikacja 1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+String mama = "Bozena";
+String Tata = "Piotr";
+String siostra = "Paulina";
